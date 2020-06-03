1ae3568b43d77ef6a191b87aac2d3c687a345981
webufoqiu <webufo@126.com>
2020-06-03 22:22:42 +0800

    koa

--- crypto/hash.js
+++ crypto/hash.js
@@ -2,8 +2,12 @@ const crypto = require('crypto');
 
 const hash = crypto.createHash('md5');
 
-hash.update('Hello, world!');
+hash.update('Hello, world!'); 
+// 6CD3556DEB0DA54BCA060B4C39479839
 hash.update('Hello, nodejs!');
+// 3F71594B0E079ECFB18AC06619BEF27D
+
+hash.update('MR.Qiu');
 
 var r = hash.digest('hex');
 console.log(`md5 hash: ${r}`); // 7e1977739c748beac0c0fd14fd26a544
